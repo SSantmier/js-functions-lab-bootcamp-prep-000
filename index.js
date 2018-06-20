35302fe47a13726b151f0731ab219449284af5f5
Sara S <ssantmier@gmail.com>
2018-06-20 15:36:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ function happyHolidays() {return "Happy holidays!"}
 
 function happyHolidaysTo(name) {return "Happy holidays ${name}!"}
 
-happyHolidaysTo("you");
-
 function happyHolidayTo(holiday, name) {return "Happy ${holiday}, ${name}!"}
 
 happyHolidayTo("Independence Day","you");
